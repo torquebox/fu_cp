0e64812a0e28ade1df960c835b0c39b324e053d2
Bob McWhirter <bob@mcwhirter.org>
2011-04-19 16:37:19 -0400

    Initial import

---
+++ fu_cp.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+puts 'Setting up'
+FileUtils.rm_rf( 'target' )
+FileUtils.mkdir_p( 'target/dirA/' )
+FileUtils.mkdir_p( 'target/dirB/' )
+open( 'target/dirA/thefile', 'w' ) {|f|
+  f.puts 'Contents of the file'
+}
+puts 'Copying'
+FileUtils.cp( 'target/dirA/thefile', 'target/dirB/thefile' )
+puts 'Copying harder'
+FileUtils.cp( 'target/dirA/thefile', 'target/dirB/thefile' )
